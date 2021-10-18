60578900da5f3cc1e1a4c45d604f4bc27896a4ce
RMossiii95 <91968093+RMossiii95@users.noreply.github.com>
2021-10-18 19:47:47 -0400

    Update Program.cs

--- MovieList/Program.cs
+++ MovieList/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace MovieList
 {
@@ -6,9 +6,50 @@ namespace MovieList
     {
         static void Main(string[] args)
         {
-            MovieDatabase MovieDb = new MovieDatabase();
+            bool goOn = true;
+            while (goOn == true)
+            {
+                MovieDatabase MovieDb = new MovieDatabase();
+                Console.WriteLine("Welcome to the Movie List!");
+                string userInput = GetUserInput("Please choose from this list of genres: SciFi, Action, Noir, and Horror!");
 
-            MovieDb.PrintMovie();
+                MovieDb.PrintMoviesByGenre(userInput);
+
+                //MovieDb.PrintMovieNames();
+
+                //MovieDb.PrintGenre();
+                goOn = Continue();
+            }
+            
+        }
+
+        public static string GetUserInput(string prompt)
+        {
+            Console.WriteLine(prompt);
+            string userinput = Console.ReadLine();
+            return userinput;
+        }
+        public static bool Continue()
+        {
+            {
+                string response = GetUserInput("Would you like to know more about another genre? y/n");
+
+                if (response == "y")
+                {
+                    return true;
+                }
+                else if (response == "n")
+                {
+                    Console.WriteLine("Good bye!");
+                    return false;
+
+                }
+                else
+                {
+                    Console.WriteLine("That was not a valid response, please try again!");
+                    return Continue();
+                }
+            }
         }
     }
 }
