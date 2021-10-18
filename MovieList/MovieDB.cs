3c8be6b6de2b6097492f1b40c04a1d853f3c0121
RMossiii95 <91968093+RMossiii95@users.noreply.github.com>
2021-10-18 19:48:48 -0400

    Update MovieDB.cs

--- MovieList/MovieDB.cs
+++ MovieList/MovieDB.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -10,27 +10,45 @@ namespace MovieList
 
         public MovieDatabase()
         {
-            ToWatch.Add(new Movie("Avatar", "SciFi"));
-            ToWatch.Add(new Movie("Star Wars", "SciFi"));
-            ToWatch.Add(new Movie("Star Trek", "SciFi"));
-            ToWatch.Add(new Movie("Saving Private Ryan", "Action"));
-            ToWatch.Add(new Movie("Avengers: Endgame", "Action"));
-            ToWatch.Add(new Movie("Fight Club", "Thriller"));
-            ToWatch.Add(new Movie("Memento", "Noir"));
             ToWatch.Add(new Movie("Alien", "Horror"));
+            ToWatch.Add(new Movie("Avatar", "SciFi"));
+            ToWatch.Add(new Movie("Avengers: Endgame", "Action"));
             ToWatch.Add(new Movie("Blade Runner", "Noir"));
+            ToWatch.Add(new Movie("Fight Club", "Noir"));
             ToWatch.Add(new Movie("Jacob's Ladder", "Horror"));
+            ToWatch.Add(new Movie("Memento", "Noir"));
+            ToWatch.Add(new Movie("Saving Private Ryan", "Action"));
+            ToWatch.Add(new Movie("Star Trek", "SciFi"));
+            ToWatch.Add(new Movie("Star Wars", "SciFi"));
+    
         }
-        public void PrintMovie()
+
+        public void PrintMoviesByGenre(string genre)
         {
-            for (int i = 0; i < ToWatch.Count; i++)
+            foreach (Movie x in ToWatch)
             {
-                Movie m = ToWatch[i];
-                Console.WriteLine($"{i} : {m.Movies}");
+                if(x.Genre == genre) 
+                {
+                    x.PrintMovie();
+                }
+
 
             }
-            Console.WriteLine();
         }
+
+        //public void PrintAllMovies()
+        //{
+        //    for (int i = 0; i < ToWatch.Count; i++)
+        //    {
+        //        Movie m = ToWatch[i];
+        //        Console.WriteLine($"{i} : {m.MovieName}");
+
+        //    }
+        //    Console.WriteLine();
+        //}
+        
+
+        
 
     }
 }
